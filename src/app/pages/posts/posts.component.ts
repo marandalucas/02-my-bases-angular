9688ba2247812dceb05ba5f5ba1a7bdc9a38542d
sockmal <sockmal@mc.local>
2021-01-30 19:17:51 +0100

    FEAT: Initial angular app

---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Data } from '@angular/router';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+    mensajes: any;
+
+  constructor(private DataService: DataService ) { }
+
+  ngOnInit() {
+
+    this.mensajes = this.DataService.getPosts();
+
+      // .subscribe( (posts: any[]) => {
+      //   console.log(posts);
+      //   this.mensajes = posts;
+      // });
+
+  }
+
+  escuchaClick( id: number ) {
+    console.log('Click en:' , id)
+  }
+
+}
